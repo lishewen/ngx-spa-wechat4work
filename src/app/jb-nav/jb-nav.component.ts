823d0f98a0deb362bf59988e65b6cd49c723dd34
算神 <lishewen@msn.com>
2018-05-28 11:20:53 +0800

    子组件属性

--- src/app/jb-nav/jb-nav.component.ts
+++ src/app/jb-nav/jb-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -9,6 +9,7 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./jb-nav.component.css']
 })
 export class JbNavComponent {
+  @Input() title: string = 'Application Title';
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
