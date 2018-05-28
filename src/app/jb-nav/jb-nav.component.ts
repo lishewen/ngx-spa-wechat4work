356e58b49fd71efd3464539e3d71a64903c37192
算神 <lishewen@msn.com>
2018-05-28 10:58:44 +0800

    fix bug

--- src/app/jb-nav/jb-nav.component.ts
+++ src/app/jb-nav/jb-nav.component.ts
@@ -10,11 +10,11 @@ import { map } from 'rxjs/operators';
 })
 export class JbNavComponent {
 
-  isHandset$: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
-    
-  constructor(private breakpointObserver: BreakpointObserver) {}
-  
-  }
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+}
